2b5b20ea61b863caeaac637d87faaf291a1dc960
DymaSv <dymasvyrydonov@gmail.com>
2021-09-07 15:52:13 -0400

    Changes for methode BinSearch

--- binsearch.go
+++ binsearch.go
@@ -2,6 +2,23 @@ package binsearch
 
 import "fmt"
 
-func BinSearch(s string) {
-	fmt.Println(s)
+func BinSearch(array *[]int, searchValue int, index int) int {
+	arr := *array
+	fmt.Println(arr)
+	if len(arr) > 1 {
+		if arr[len(arr)/2] == searchValue {
+			return len(arr) / 2
+		} else if arr[len(arr)/2] > searchValue {
+			slice1 := arr[0 : len(arr)/2]
+			return BinSearch(&slice1, searchValue, index)
+		} else if arr[len(arr)/2] < searchValue {
+			slice2 := arr[len(arr)/2 : len(arr)-1]
+			return index + BinSearch(&slice2, searchValue, len(arr)/2)
+		}
+	} else {
+		if arr[0] == searchValue {
+			return 0
+		}
+	}
+	return -1
 }
